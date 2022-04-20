f5ff83321020d39c8f8a87f4ca1fda5c46cb35d1
some-earth11 <sk8762@srmist.edu.in>
2022-04-20 22:13:25 +0530

    fixed footer

--- comps/Footer.js
+++ comps/Footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
     return ( 
         <footer>
-            Copyright 2022 <Link href="https://github.com/some-earth11"><a>@some-earth11</a></Link>
+            Copyright 2022 <Link href="https://github.com/some-earth11"><a target="_blank" rel="noopener noreferrer">@some-earth11</a></Link>
         </footer>
     );
 }
--- comps/Navbar.js
+++ comps/Navbar.js
@@ -5,9 +5,8 @@ const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
-                <Image src="/logo.png" width={60} height={60}></Image>
+                <Image src="/logo.png" width={60} height={60}></Image>           
             </div>
-            {/* <h1>ManCity Wiki</h1> */}
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
             <Link href="/players"><a>Players</a></Link>
