a976827bba059420935233b9438cc0e19effc158
some-earth11 <sk8762@srmist.edu.in>
2022-04-20 15:26:45 +0530

    API fetch created

--- pages/players/index.js
+++ pages/players/index.js
@@ -1,9 +1,27 @@
-const Ninjas = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { players : data }
+    }
+}
+
+
+
+const Players = ({ players} ) => {
     return ( 
         <div>
             <h1>All Players</h1>
+            {players.map(player => (
+                <div key={player.id}>
+                    <a>
+                        <p>{ player.name }</p>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
     
-export default Ninjas;
+export default Players;
