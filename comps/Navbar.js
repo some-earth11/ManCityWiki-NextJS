5e4aa16d9696398b274ff103f7946508c4e00595
some-earth11 <sk8762@srmist.edu.in>
2022-04-20 16:03:04 +0530

    fixed navbar

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = () => {
         <nav>
             <div className="logo">
                 <Image src="/logo.png" width={60} height={60}></Image>
-                <h1>ManCity Wiki</h1>
-                <Link href="/"><a>Home</a></Link>
-                <Link href="/about"><a>About</a></Link>
-                <Link href="/players"><a>Players</a></Link>
             </div>
+            {/* <h1>ManCity Wiki</h1> */}
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/players"><a>Players</a></Link>
         </nav>
     );
 }
